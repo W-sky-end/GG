5dd28ad4de884298fe95b9b45e7bc5ce180730f5
Vadim <aonyko@gmail.com>
2026-07-17 14:05:26 +0300

    Com  ini

---
+++ src/main/java/dungeonbot/Main.java
@@ -0,0 +1,32 @@
+package dungeonbot;
+
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String botToken = loadBotToken();
+
+        TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+        botsApplication.registerBot(botToken, new DungeonBot(botToken));
+
+        System.out.println("Бот запущен!");
+
+        Thread.currentThread().join();
+    }
+
+    private static String loadBotToken() throws IOException {
+        Properties properties = new Properties();
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new IOException("Файл config.properties не найден в resources!");
+            }
+            properties.load(input);
+        }
+        return properties.getProperty("bot.token");
+    }
+}
